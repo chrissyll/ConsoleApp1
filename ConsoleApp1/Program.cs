437d31f5019f95dcec9a495d371411b7c2d366bc
chrissyll <41743800+chrissyll@users.noreply.github.com>
2018-07-31 15:10:23 +0800

    111
    2222

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MSIT120" + "First String");
+            Console.WriteLine("Hello, World");
+            Console.WriteLine("上");
+            Console.WriteLine("中");
+            Console.WriteLine("下");
+        }
+    }
+}
